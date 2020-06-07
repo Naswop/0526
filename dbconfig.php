c5f40c355ee94a4d7bb9abe13349b6251d557faf
Naswop <329835906@qq.com>
2020-06-07 19:06:47 +0800

    0607

---
+++ dbconfig.php
@@ -0,0 +1,13 @@
+<?php  
+define('DB_HOST', '127.0.0.1');
+//端口号
+define('DB_PORT', '3306');
+
+// 用户名
+define('DB_USER', 'zbwxh_ip1a_com');
+
+// 密码
+define('DB_PWD', 'dXeckGGy7BiAnRcp');
+
+// 数据库名
+define('DB_NAME', 'zbwxh_ip1a_com');
